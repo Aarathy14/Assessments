259a4f1cab7bc9f3051147e766d56eb89431a080
Aarathy14 <aarathyganesh14@gmail.com>
2022-04-04 13:05:05 +0530

    'assessment1'

---
+++ Assessment1/src/Project1.js
@@ -0,0 +1,391 @@
+import React from 'react';
+import { GoMail } from "react-icons/go";
+import { BiPhoneCall } from "react-icons/bi";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { BiBasketball } from "react-icons/bi";
+import { TiSocialGooglePlus } from "react-icons/ti";
+import { ImPinterest2 } from "react-icons/im";
+import { AiOutlineLeft } from "react-icons/ai";
+import icon from './Photos/icon.png';
+import allicon from './Photos/allicon.png';
+import { BiBed } from "react-icons/bi";
+import { GiBathtub } from "react-icons/gi";
+import { MdSquareFoot } from "react-icons/md";
+import { BsArrowRepeat } from "react-icons/bs";
+import { FiHeart } from "react-icons/fi";
+import building1 from './Photos/building1.jpg';
+import building2 from './Photos/building2.jpg';
+import building3 from './Photos/building3.jpg';
+import { BsPlusCircle } from "react-icons/bs";
+import muniba from './Photos/muniba.jpg';
+import sign from './Photos/sign.jpg';
+import { ImQuotesLeft } from "react-icons/im";
+import line from './Photos/line.png';
+import v from './Photos/v.webp';
+import ysh from './Photos/ysh.jpg';
+import Suriya from './Photos/Suriya.jpg';
+import jk from './Photos/jk.jpg';
+import ji from './Photos/ji.webp';
+import { VscDebugStackframeDot } from "react-icons/vsc";
+import house1 from './Photos/house1.jpg';
+import house2 from './Photos/house2.jpg';
+import house3 from './Photos/house3.jpg';
+import house4 from './Photos/house4.jpg';
+import house5 from './Photos/house5.jpg';
+import color from './Photos/color.jpg';
+import color1 from './Photos/color1.jpg';
+import {BsDot  } from "react-icons/bs";
+import {AiFillDribbbleCircle} from "react-icons/ai";
+import{AiFillBehanceCircle}from "react-icons/ai";
+import './Project.css';
+function Project1() {
+  return (
+    <div>
+      <div className='content'>
+        <div class='content1'>
+          <GoMail size='30' />
+          sample@sample.com</div>
+        <div className='phone'>
+          <BiPhoneCall size='20' />
+          0012345678
+        </div>
+        <div className='icon'>
+          <ImPinterest2 size='30' />
+          <TiSocialGooglePlus size='30' />
+          <TiSocialTwitterCircular size='30' />
+          <RiFacebookCircleLine size='30' />
+          <BiBasketball size='30' />
+        </div>
+      </div>
+      <div className='both'>
+        <div className="real">
+          <img src={icon}></img>
+        </div>
+        <div className='Word'>
+          <div className='all'> HOME</div>
+          <div className='all1' >ABOUT</div>
+          <div className='all2'>AGENTS </div>
+          <div className='all3'>PROPERTIES </div>
+          <div className='all4'> BLOG</div>
+          <div className='all5'>CONTACT</div>
+
+        </div>
+      </div>
+      <div className='family'>
+        <div className='note'>
+          If you can dream, we can create it
+        </div>
+        <div className='note1'>
+          Find your house without any  difficulties
+        </div>
+        <div className='button'>
+          <button className='btn btn-light'> Know More</button></div>
+
+        <div className='box'>
+          01|05.....
+          <div className='tag'>
+            <AiOutlineLeft size="20" />
+          </div>
+        </div>
+      </div>
+
+      <div className='allicon'>
+        <img src={allicon} ></img>
+
+      </div>
+      <div className='real1'>
+        <img src={icon}></img>
+      </div>
+      <div className='a'>
+        Properties    </div>
+      <div className='b'>HOME FOR SALE
+      </div>
+      <div className='build1'>
+        <div className='c'>
+          
+          <h5>
+          Scandinavian style apartment
+          94-98 Ingraham st,Brooklyn,NY 11237,USA </h5>
+          <div className='common'>
+            <div className='a1'>
+              <BiBed size='15' /></div>
+            <div className='a2' ><h6>bed</h6></div>
+            <div className='common1'>
+              <GiBathtub size='15' /> </div>
+            <div className='b1'>
+              <h6>bathroom</h6>
+            </div>
+            <div className='common2 '>
+              <MdSquareFoot size='20' />
+              <div className='c1'><h6>Sq.ft;500</h6> </div>
+            </div>
+            <div className='common3'>
+              <h5>for rent</h5>  <h6>$54000</h6>  </div>
+            <div className='common4'> <div className='d1'>
+              <BsArrowRepeat size="15" /> </div>
+              <div className='d2'>
+                <FiHeart size="15" /> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='three'>
+
+        <div className='building1 '><img src={building1} width="300" height="300"></img></div>
+        
+        <div className='building2'><img src={building2} width="350" height="300"></img></div>
+        
+        <div className='building3'> <img src={building3} width="350" height="300"></img></div>
+       
+      </div>
+      <div className='add'>
+        <h6> Scandinavian style apartment <br />
+          94-98 ingraham st,brooklyn,NY 11237,USA
+        </h6></div>
+      <div className='= add1'>
+        <h6> Lovelace Road Greenfield <br />
+          120 NSt SW    Thomaston, Georgia(GA),30286
+        </h6></div>
+      <div className='add2'>
+        <h6> Building Complex Apartments <br />
+          319 Brookmeade Way Lawreceville, Georgia(GA),30043
+        </h6>
+      </div>
+      <hr></hr>
+
+      <div className='click'>
+        <BsPlusCircle size="30" /></div>
+      <div className='click1'>
+        view more</div>
+
+      <div className='design1'>
+        Beautifully designed,inside and out,<br /> studio looks every bit as powerful as it is. </div>
+      <div className='design2'> This is photoshop's versio of lorem ipsum.proin gravida nibh vel velit auctor aliquest.<br />
+        Aenean sollicitidin,lorem quin bibendum auctor,nsi elit consequat ,nec segithis sem nibh id llit.<br />Duis sed odio sit ament nibh vulputate curus a sit amet mauris.
+      </div>
+      <div className='design3'>
+        <img src={muniba} width="500" height="500" />
+      </div>
+      <div className='sign'>
+        <img src={sign} width="100" height="60" /> </div>
+      <div className='client'>
+        <h2>Our Clients Review </h2>
+      </div>
+      <div className='name'>
+        <h4>Muniba Mazari</h4>
+      </div>
+
+      <div className='name1'>
+        <h6>Product Designer,USA</h6></div>
+      <div className='name3'>
+        I'm a freelance fashion designer who specialises in print designs and combining fabrics.My designs have been sold all over Europe and
+        the USA and</div>
+       
+       <div className='name4'>
+       I have worked with some of the biggest designers in the industry. Europe and the USA and I have worked with some 
+       of the biggest </div>
+       
+        <div className='name5'>
+        designers in the industry. I'm also currently working with abig supermarket to
+        develop a gorgeous, yet affordable.  </div>
+      
+      <div className='e'>
+        <ImQuotesLeft size="35" /> </div>
+      <div className='line'>
+        <img src={line} ></img></div>
+      <div className='face'>
+        <img src={v} width="100" ></img>
+      </div>
+      <div className=' face1'>
+        <img src={ysh} width="100" ></img>
+      </div>
+      <div className='face2'>
+        <img src={Suriya} width="100" height="100" ></img>
+      </div>
+      <div className='face3'>
+        <img src={jk} width="100" height="90"></img>
+      </div>
+      <div className='face4'>
+        <img src={ji} width="100" height="90"></img>
+      </div>
+      <div className='dot'>
+        <VscDebugStackframeDot size="30" />
+        <VscDebugStackframeDot size="30" />
+        <VscDebugStackframeDot size="30" color="blue" />
+        <VscDebugStackframeDot size="30" />
+        <VscDebugStackframeDot size="30" />
+      </div>
+<div className='blog'>
+ <h2>Our Blog</h2> 
+</div>
+<div className='house1'>
+<img src={house1} width="500" height="700" ></img>
+</div>
+<div className='house2'>
+<img src={house2} width="350" height="350" ></img>
+</div>
+<div className='house3'>
+<img src={house3} width="350"height="330" ></img>
+</div>
+<div className='house4'>
+<img src={house4} width="350" height="350" ></img>
+</div>
+<div className='house5'>
+<img src={house5} width="350" height="330" ></img>
+</div>
+<div className='f1'>
+  Loream ipsum is simply dummy text of <br/>the printing and typesetting
+</div>
+<hr className='f2'></hr>
+<div className='f3'>
+  13 March,2020
+</div>
+<div className='f4'>
+  Contrarty to popular belief, Loream ipsum is not simply random text.<br/>
+  It has roots in a piece of classical latin literature from
+
+</div>
+<div className='g1'>
+  There are many variations of<br/>
+   passages of Loream ipsum
+</div>
+ <hr  className='g2'></hr>
+<div className='g3'>
+  13 March,2020
+</div>
+
+<div className='H1'>
+There are many variations of<br/>
+   passages of Loream ipsum
+</div>
+<hr  className='h2'></hr>
+<div className='H3'>
+13 March,2020
+</div>
+<div className='i1'>
+There are many variations of<br/>
+   passages of Loream ipsum</div>
+   <hr  className='i2'></hr>
+   <div className='I3'>
+ 13 March,2020</div>
+ <div className='j1'>
+ There are many variations of<br/>
+   passages of Loream ipsum
+ </div>
+<hr className='j2'></hr> 
+<div className='j3'>
+13 March,2020
+</div>
+<div className='blue'>
+<div className='every'>
+  Every thing is about to change.
+</div>
+<div className='thing'>
+Lorem ipsum dolor sit amet, condectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore
+</div>
+<div className='thing1'>
+ magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+</div>
+<div className='email'>
+  <input type="text" className='formcontrol  w-50 p-2' placeholder='Email Address'></input>
+</div>
+<div className='k'> 
+<button className='k1' >SUBSCRIBE</button></div>
+</div>
+<div className='fullhouse'>
+</div>
+<div className='l'>WP</div>
+<div class='L1'>/</div>
+<div className='l2'>Real Estate</div>
+<div className='m'>
+<div className='m1'>HOME</div>
+<div className='m2'>AGENTS</div>
+<div className='m3'>PROPERTIES</div>
+<div className='m4'>BLOG</div>
+<div className='m5'>CONTACT</div>
+</div>
+
+<div className='n1'> ABOUT</div>
+<div className='n2'>ABOUT STUDIO </div>
+<div className='n2'>JOBS </div>
+<div className='n2'>TEAM </div>
+<div className='n2'>SECURITY </div>
+
+<div  className='o'> INFORMATION</div>
+<div  className='o1'>  CUSTOMER SERVICE</div>
+<div  className='o2'> LEGAL INFORMATION</div>
+<div  className='o3'> SUPPORT</div>
+<div  className='o4'> FAQ</div>
+
+<div   className='p'>  RECENT NEWS  </div>
+<div className='p1'>
+<img src={color} width="100" ></img>
+</div>
+<div className='p2'>
+<img src={color1} width="100" ></img>
+</div>
+<div className='p3'> 
+Proin eros nisl,ornare sed <br/>
+vehicula sit amet,feugiat</div>
+<div class="p4">
+Aug 12| 2017
+</div>
+<div className='p5'>
+Morbi placerat accumsan<br/>
+nunc,luctus lacinia
+</div>
+<div className='p6'>
+ Aug12|2017
+</div>
+<div className='p7'>
+GET IN TOUCH
+</div>
+<div className='p8'>
+<BsDot size="30"/>
+</div>
+<div className='p9'>
+San Fransisco,CA<br/>
+125,califorina,STP 2 Building
+</div>
+<div className='p10'>
+<BsDot size="30"/>
+</div>
+<div className='p11'>
+WWW.studiotheme.com
+</div>
+<div className='p12'>
+<BsDot size="30"/>
+</div>
+<div className='p13'>
++12356458934
+</div>
+<div className='black'></div>
+
+<div className='q1'>
+<AiFillDribbbleCircle size="30"/>
+<TiSocialGooglePlus size='30' />
+ <TiSocialTwitterCircular size='30' />
+ <AiFillBehanceCircle size='30'/>
+</div>
+<div className='q2'>
+<h5>PRIVARY</h5>
+</div>
+<div className='q3'>
+<h5>|</h5>
+</div>
+<div className='q4'>
+<h5>TERM & CONDITION</h5>
+</div>
+
+
+    </div>
+
+
+
+  )
+}
+
+
+export default Project1
